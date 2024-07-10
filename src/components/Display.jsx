fb4971daff6fcd986fa157867b8cdbafcf607a57
Gyanendra <gyanen56@gmail.com>
2024-07-10 13:56:41 +1000

    filter btns

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react"
 import MovieCard from "./MovieCard"
 
 const Display = ({ genres }) => {
-  
+  const [filterMovies, setFilterMovies] = useState([])
+ const handleFilterBtns = (str)=>{
+    if(str==='all'){
+      setFilterMovies([...genres])
+      return
+    }
+      const items = genres.filter((item)=>item.category===str)
+      setFilterMovies(items)
+  }
+  useEffect(()=>{
+    setFilterMovies(genres)
+  },[genres])
+  console.log(filterMovies)
   return (
     <div className="pt-5 display container bg-dark ">
       <div className="row  rounded text-light p-3">
@@ -11,20 +24,34 @@ const Display = ({ genres }) => {
             role="group"
             aria-label="Basic mixed styles example"
           >
-            <button type="button" className="btn btn-primary">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={() => handleFilterBtns('all')}
+            >
               All
             </button>
-            <button type="button" className="btn btn-warning">
+            <button
+              type="button"
+              className="btn btn-warning"
+              onClick={() => handleFilterBtns('drama')}
+            >
               Drama
             </button>
-            <button type="button" className="btn btn-info">
+            <button
+              type="button"
+              className="btn btn-info"
+              onClick={() => handleFilterBtns('action')}
+            >
               Action
             </button>
           </div>
         </div>
+        <h5 className="mt-3">{filterMovies.length} Movies Listed</h5>
       </div>
+
       <div className="d-flex justify-content-around flex-wrap row-gap-2 mt-5 pb-5">
-        {genres.map((genre,i)=>{
+        {filterMovies.map((genre, i) => {
           return <MovieCard foundMovie={genre} key={i} />
         })}
       </div>
